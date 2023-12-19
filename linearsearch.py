88f1dc278661a1467774e7b09463e26c8e57cdff
Ellertone <ellertonemain@gmail.com>
2023-12-19 16:33:03 +0300

    Binary

--- linearsearch.py
+++ linearsearch.py
@@ -6,3 +6,15 @@ def linear_search(list, target):
     for i in range(0, len(list)):
         if list[i] == target:
             return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target is found at index ", index  )
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,4,6,7,8,5,8,5,8,6]
+
+result = linear_search(numbers,12)
+verify(result)
